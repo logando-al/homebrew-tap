92ca2e4b69e6bcc50b100c133c08632e2486e506
logando-al <luqmann1972@gmail.com>
2026-03-02 09:05:57 +0800

    Fix ani-tui checksums
    Co-Authored-By: Lugina <lugina@ltechnosoft.com>

--- Formula/ani-tui.rb
+++ Formula/ani-tui.rb
@@ -5,10 +5,10 @@ class AniTui < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/logando-al/ani-tui/releases/download/v1.0.4/ani-tui-macos-aarch64"
-    sha256 "1f1ab55408653d82f3d4f6504053fb7585391df2613192518055bfcbf41cf397"
+    sha256 "70fb95cb15ae08a98301b2c075701f1c6dbf7b293c3a9e442c0920ad8feaa637"
   else
     url "https://github.com/logando-al/ani-tui/releases/download/v1.0.4/ani-tui-macos-x86_64"
-    sha256 "e8ab05236332226719b6160dc764bf8bb751fdf518ee64f9c8ef4409d1d1ef5d"
+    sha256 "84d7bc0a3ecddc2f5c7dc02399c2b3c8384ccc2d344cce33d8cf0088596208d7"
   end
 
   def install
